26429c69367ecff5aa2a2c979ca0fd71f4be08e9
David Lambauer <david@run-as-root.sh>
2026-04-14 18:29:12 +0200

    feat: add fakerphp/faker dependency and FakerFactory service

---
+++ src/Service/FakerFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DavidLambauer\Seeder\Service;
+
+use Faker\Factory;
+use Faker\Generator;
+
+class FakerFactory
+{
+    public function create(string $locale = 'en_US', ?int $seed = null): Generator
+    {
+        $faker = Factory::create($locale);
+
+        if ($seed !== null) {
+            $faker->seed($seed);
+        }
+
+        return $faker;
+    }
+}
---
+++ tests/Unit/Service/FakerFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DavidLambauer\Seeder\Test\Unit\Service;
+
+use DavidLambauer\Seeder\Service\FakerFactory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+final class FakerFactoryTest extends TestCase
+{
+    public function test_creates_faker_with_default_locale(): void
+    {
+        $factory = new FakerFactory();
+        $faker = $factory->create();
+
+        $this->assertInstanceOf(Generator::class, $faker);
+    }
+
+    public function test_creates_faker_with_custom_locale(): void
+    {
+        $factory = new FakerFactory();
+        $faker = $factory->create('de_DE');
+
+        $this->assertInstanceOf(Generator::class, $faker);
+    }
+
+    public function test_creates_deterministic_faker_with_seed(): void
+    {
+        $factory = new FakerFactory();
+        $faker1 = $factory->create('en_US', 42);
+        $name1 = $faker1->name();
+
+        $faker2 = $factory->create('en_US', 42);
+        $name2 = $faker2->name();
+
+        $this->assertSame($name1, $name2);
+    }
+
+    public function test_creates_random_faker_without_seed(): void
+    {
+        $factory = new FakerFactory();
+        $faker = $factory->create('en_US', null);
+
+        $this->assertNotEmpty($faker->name());
+    }
+}
